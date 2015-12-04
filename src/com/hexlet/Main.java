adc988c177e395ce8740869571a9202083d7cbe7
templer <templer5173902@mai.ru>
2015-12-04 18:28:35 +0200

    init

---
+++ src/com/hexlet/Main.java
@@ -0,0 +1,11 @@
+package com.hexlet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        User user1 = new User("Dima", 21);
+        User user2 = new User("Sasha", 20);
+        System.out.println(user1 + "\n" + user2);
+    }
+
+}
