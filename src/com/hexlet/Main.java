5ac281c70ae47406b67ef714b39beb18a9dd1642
templer <templer5173902@mai.ru>
2015-12-04 19:06:56 +0200

    merge 1

--- src/com/hexlet/Main.java
+++ src/com/hexlet/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         User user1 = new User("Dima", 21);
+<<<<<<< HEAD
         User user2 = new User("Sasha", 20);
         User user3 = new User("Anna", 20);
         System.out.println(user1 + "\n" + user2 + "\n" + user3);
+=======
+        System.out.println(user1);
+>>>>>>> master
     }
 
 }
