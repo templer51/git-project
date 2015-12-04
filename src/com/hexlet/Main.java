53fd2cb6f6309b55535dce2dca73756ee90a6999
templer <templer5173902@mai.ru>
2015-12-04 18:31:18 +0200

    second commit

--- src/com/hexlet/Main.java
+++ src/com/hexlet/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         User user1 = new User("Dima", 21);
-        User user2 = new User("Sasha", 20);
-        System.out.println(user1 + "\n" + user2);
+        System.out.println(user1);
     }
 
 }
