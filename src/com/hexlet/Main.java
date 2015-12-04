5c11b6ae2b861e6dd156bf07564ae2f70f6c1c44
templer <templer5173902@mai.ru>
2015-12-04 18:34:57 +0200

    addUser branch

--- src/com/hexlet/Main.java
+++ src/com/hexlet/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         User user1 = new User("Dima", 21);
         User user2 = new User("Sasha", 20);
-        System.out.println(user1 + "\n" + user2);
+        User user3 = new User("Anna", 20);
+        System.out.println(user1 + "\n" + user2 + "\n" + user3);
     }
 
 }
